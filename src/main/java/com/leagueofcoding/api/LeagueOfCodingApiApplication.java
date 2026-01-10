f8d09bb7a5feb0e87d065526f261789102ede526
dao-nguyenminh <daonguyenminh.it@gmail.com>
2026-01-10 23:21:29 +0700

    chore: initial Spring Boot project setup - Spring boot 3.5.9 - Java 21 - Dependencies: Web, JPA, PostgreSQL, Lombok - Maven project structure

---
+++ src/main/java/com/leagueofcoding/api/LeagueOfCodingApiApplication.java
@@ -0,0 +1,13 @@
+package com.leagueofcoding.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfCodingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfCodingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leagueofcoding/api/LeagueOfCodingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leagueofcoding.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfCodingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
